f388a5010b8f01b6ee0014ab71ebfda1cdb3c3f7
Mann Barot <manbar@unc.edu>
2025-03-29 17:20:31 -0400

    Save progress on footprint page

--- gen-ai-footprint/src/app/page.tsx
+++ gen-ai-footprint/src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
+import footprintPage from "./footprintPage/page";
 
 export default function Home() {
   return (
-    <>
-    <h1>Your AI Footprint is AWFUL!!!</h1>
-    </>
+
+    <div>
+    {/* // <h1>Your AI Footprint is AWFUL!!!</h1>
+    // <div className="space-y-4">
+    //     <Link href="/footprintPage">
+    //       Calculate Your Footprint
+    //     </Link>
+    //     </div> */}
+       </div>
+    
   );
 }

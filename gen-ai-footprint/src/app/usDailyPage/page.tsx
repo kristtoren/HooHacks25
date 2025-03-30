4252db0279c1c8bfc58bcebc8ec30819a4330fa7
Mann Barot <manbar@unc.edu>
2025-03-30 11:15:58 -0400

    edited daily stuff page

--- gen-ai-footprint/src/app/usDailyPage/page.tsx
+++ gen-ai-footprint/src/app/usDailyPage/page.tsx
@@ -3,21 +3,26 @@ import { EnergySourcesPie } from "@/components/charts/USDailyFootprint/USEnergyS
 
 export default function USDailyPage() {
   return (
-    <div className="flex flex-col w-full">
-        <div className="flex-1 px-10 py-10 w-2/3">
-            <div className="flex flex-row md:flex-row gap-6 ml-[-210px] mt-[-45px] mb-20 w-full">
-                <div className="flex-1">
-                <EnergySourcesPie />
-                </div>
-                <h3 className="mt-10 text-1xl sm:text-2xl md:text-4xl lg:text-5xl font-extrabold text-center text-blue-500 animate-fade-in mb-6 mt-[-30px] w-300 flex-1">
-                    This data shows that nearly three-quarters of U.S. energy consumption is fueled by nonrenewable sources—petroleum and natural gas. In a similar way, the massive data centers powering ChatGPT require enormous amounts of energy for training and operation. Often, these data centers are powered by the same types of nonrenewable sources, contributing to a significant carbon footprint.
-                </h3>
-            </div>
-            <div className="flex-1">
-                <ChatGPTWeeklyUsersLine />
-            </div>
-        </div>
+    <div className="flex flex-col w-full px-2 md:px-5 gap-10">
+      
+      {/* Text at the top */}
+      <div className="max-w-4xl mx-auto text-center">
+        <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-mono text-fuchsia-300 animate-fade-in">
+          This data shows that nearly three-quarters of U.S. energy consumption is fueled by nonrenewable sources—petroleum and natural gas. In a similar way, the massive data centers powering ChatGPT require enormous amounts of energy for training and operation. Often, these data centers are powered by the same types of nonrenewable sources, contributing to a significant carbon footprint.
+        </h3>
+      </div>
+      <div className="flex flex-row  gap-8 justify-center">
+        {/* Chart 1 */}
+      <div className="w-full">
+        <EnergySourcesPie />
+      </div>
+
+      {/* Chart 2 */}
+      <div className="w-full">
+        <ChatGPTWeeklyUsersLine />
+      </div>
+      </div>
+      
     </div>
-        
   );
 }
